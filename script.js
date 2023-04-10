027d58b7d83967e67e6c61796dc450d6c8cc2608
Bell <skyfall.kay.team@gmail.com>
2023-04-10 02:52:51 -0400

    project links and fixs
    - added the 6 main projects
    - added links on projects
    - fixed the height problem of studies
    - fixed le position of work experiencies
    - added footer fixed position on scroll

--- script.js
+++ script.js
@@ -26,6 +26,26 @@ const TRAFIC_CNN = "https://github.com/victor-mira/traffic-signs-cnn"
 const CHESS_ENGINE = "https://github.com/victor-mira/Bell-Chess-Engine"
 const PORTFOLIO = "https://github.com/victor-mira/portfolio-raw"
 
-const project_OpenCVDetector = document.getElementById("openCVDetector")
+const project_OpenCVDetector = document.getElementById("opencv-detector")
+const project_OpenCVSegmentation = document.getElementById("opencv-segmentation")
+const project_Optiski = document.getElementById("optiski")
+const project_AnimeVAE = document.getElementById("anime-vae")
+const project_TraficCNN = document.getElementById("trafic-cnn")
+const project_Portfolio = document.getElementById("portfolio")
 
 project_OpenCVDetector.addEventListener("click", () => window.open(OPENCV_DETECTOR))
+project_OpenCVSegmentation.addEventListener("click", () => window.open(OPENCV_SEGMENTATION))
+project_Optiski.addEventListener("click", () => window.open(OPTISKI))
+project_AnimeVAE.addEventListener("click", () => window.open(ANIME_VAE))
+project_TraficCNN.addEventListener("click", () => window.open(TRAFIC_CNN))
+project_Portfolio.addEventListener("click", () => window.open(PORTFOLIO))
+
+// Footer
+const footer = document.querySelector("footer")
+
+function fixFooter() {
+    footer.style.position = "fixed"
+    footer.style.bottom = "0"
+}
+document.addEventListener("scroll", fixFooter)
+
